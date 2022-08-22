e8fe2cb605112e20a8e04297a83e7ca80bb1bb73
Allan <kipropsallan@gmail.com>
2022-08-22 15:00:47 +0300

    commit tree 18ae7523d502354290c891d6c9f3b6194182cbd5

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,20 @@
-class Dog
+require 'pry'
 
+class Dog
+    def name=(name)
+        @name = name
+    end
+
+    def breed=(breed)
+        @breed = breed
+    end
+
+    def name
+        @name
+    end
+
+    def breed
+        @breed
+    end
 end
+
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,20 @@
+require 'pry'
+
 class Person
+    def name=(name)
+        @name = name
+    end
+
+    def job=(job)
+        @job = job
+    end
+
+    def name
+        @name
+    end
+
+    def job
+        @job
+    end
 
 end
